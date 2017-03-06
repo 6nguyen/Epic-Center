95fbc337b30ca17287a6ec71e559f11c9a8e9504
George Nguyen <g.nguyen92@yahoo.com>
2017-03-06 15:08:30 -0800

    Update EarthquakeAdapter to format mMagnitude with 1 decimal place.

--- app/src/main/java/com/example/gnguy/epiccenter/EarthquakeAdapter.java
+++ app/src/main/java/com/example/gnguy/epiccenter/EarthquakeAdapter.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -34,55 +35,58 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         // Find the earthquake at the given position
         Earthquake currentEarthquake = getItem(position);
 
+
+        // Format and display magnitutde to one decimal place
+        double magnitude = currentEarthquake.getMagnitutude();
+        DecimalFormat magFormatter = new DecimalFormat("0.0");
+        String formattedMag = magFormatter.format(magnitude);
+
         // Find the TextView with the ID magnitude
         TextView magnitudeView = (TextView)listItemView.findViewById(R.id.magnitude);
         // Display magnitude of current earthquake into above TextView
-        magnitudeView.setText(currentEarthquake.getMagnitutude());
+        magnitudeView.setText(formattedMag);
 
 
         // Convert the String mLocation into two Strings, a primary location and location offset
         String location = new String(currentEarthquake.getLocation());
-        String locationOffset = null;
-        String primaryLocation = null;
-        if (location.contains(" of ")){
-            int endIndex = location.indexOf(" of") + 4;
-            locationOffset = location.substring(0,endIndex);
-            primaryLocation = location.substring(endIndex, location.length());
-        } else {
-            locationOffset = "Near the ";
-            primaryLocation = location;
-        }
+            String locationOffset = null;
+            String primaryLocation = null;
+            if (location.contains(" of ")){
+                int endIndex = location.indexOf(" of") + 4;
+                locationOffset = location.substring(0,endIndex);
+                primaryLocation = location.substring(endIndex, location.length());
+            } else {
+                locationOffset = "Near the ";
+                primaryLocation = location;
+            }
 
-        // Location offset
-        TextView locationOffsetView = (TextView)listItemView.findViewById(R.id.location_offset);
-        locationOffsetView.setText(locationOffset);
+            // Location offset
+            TextView locationOffsetView = (TextView)listItemView.findViewById(R.id.location_offset);
+            locationOffsetView.setText(locationOffset);
 
-        // Find the TextView with the ID location
-        TextView primaryView = (TextView)listItemView.findViewById(R.id.primary_location);
-        // Display location of current earthquake into above TextView
-        primaryView.setText(primaryLocation);
+            // Primary location
+            TextView primaryView = (TextView)listItemView.findViewById(R.id.primary_location);
+            primaryView.setText(primaryLocation);
 
 
         // Convert the long mTimeInMilliseconds to a String with date format
         Date dateObject = new Date(currentEarthquake.getmTimeInMilliseconds());
-        SimpleDateFormat dateFormatter = new SimpleDateFormat("MMM DD, yyyy");
-        String date = dateFormatter.format(dateObject);
+            SimpleDateFormat dateFormatter = new SimpleDateFormat("MMM DD, yyyy");
+            String date = dateFormatter.format(dateObject);
 
-        // Find the TextView with the ID date
-        TextView dateView = (TextView)listItemView.findViewById(R.id.date);
+            // Find the TextView with the ID date
+            TextView dateView = (TextView)listItemView.findViewById(R.id.date);
+            // Display date of current earthquake into above TextView
+            dateView.setText(date);
 
-        // Display date of current earthquake into above TextView
-        dateView.setText(date);
+            // Convert the timeInMilliseconds to a String with time format
+            SimpleDateFormat timeFormatter = new SimpleDateFormat("h:mm a");
+            String time = timeFormatter.format(dateObject);
 
-        // Convert the timeInMilliseconds to a String with time format
-        SimpleDateFormat timeFormatter = new SimpleDateFormat("h:mm a");
-        String time = timeFormatter.format(dateObject);
-
-        // Find the TextView with the ID time
-        TextView timeView = (TextView)listItemView.findViewById(R.id.time);
-
-        // Display time of current earthquake into above TextView
-        timeView.setText(time);
+            // Find the TextView with the ID time
+            TextView timeView = (TextView)listItemView.findViewById(R.id.time);
+            // Display time of current earthquake into above TextView
+            timeView.setText(time);
 
 
         return listItemView;
