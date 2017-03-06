7bdc3d920cc89ae0f0f332a54df291352edeb4ac
George Nguyen <g.nguyen92@yahoo.com>
2017-03-06 13:25:30 -0800

    Remove placeholder data from MainActivity

--- app/src/main/java/com/example/gnguy/epiccenter/MainActivity.java
+++ app/src/main/java/com/example/gnguy/epiccenter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.gnguy.epiccenter;
 
+import android.app.DownloadManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -17,15 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Fake list of earthquake locations
-        ArrayList<Earthquake> earthquakes = new ArrayList<Earthquake>();
-
-        earthquakes.add(new Earthquake("7.4","San Francisco","Jan 16, 2017"));
-        earthquakes.add(new Earthquake("5.4","Tustin","Sep 8, 2016"));
-        earthquakes.add(new Earthquake("9.8","Los Angeles","Dec 21, 2012"));
-        earthquakes.add(new Earthquake("9.9", "Tustin", "May 14, 1992"));
-        earthquakes.add(new Earthquake("4.5", "San Jose", "Mar 24, 2015"));
-        earthquakes.add(new Earthquake("5.1", "Sacramento", "Nov 19, 2016"));
-        earthquakes.add(new Earthquake("4.6", "Liverpool", "Dec 24, 2014"));
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
 
         // Find a reference to the {@link ListView} in the layout
         ListView listView = (ListView)findViewById(R.id.list_view);
@@ -35,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the adapter onto the {@link ListVIew} so the list can be populated in the UI
         listView.setAdapter(adapter);
-
 
     }
 }
