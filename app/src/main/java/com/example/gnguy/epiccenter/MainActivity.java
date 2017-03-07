7ee29ef90e6430ff5304939b99fa3b03b8595a57
George Nguyen <g.nguyen92@yahoo.com>
2017-03-07 14:10:04 -0800

    Set listItems to open earthquake URL when clicked on.

--- app/src/main/java/com/example/gnguy/epiccenter/MainActivity.java
+++ app/src/main/java/com/example/gnguy/epiccenter/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.gnguy.epiccenter;
 
-import android.app.DownloadManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,17 +20,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Fake list of earthquake locations
+        // Extraxt list of earthquake details
         ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
 
         // Find a reference to the {@link ListView} in the layout
         ListView listView = (ListView)findViewById(R.id.list_view);
 
         // Create a new {@link ArrayAdapter} of earthquakes
-        EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
+        final EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
 
         // Set the adapter onto the {@link ListVIew} so the list can be populated in the UI
         listView.setAdapter(adapter);
+
+        // Set listView items to open earthquake url when clicked
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Find the earthquake at the given position
+                Earthquake currentEarthquake = adapter.getItem(position);
+
+                // Parse mUrl to a readable Uri
+                Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
+
+                // Set a new intent to open the web page using any browser
+                // give option to user which browser will open, or default browser
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+
+                // Open browser activity
+                startActivity(browserIntent);
+            }
+        });
 
     }
 }
