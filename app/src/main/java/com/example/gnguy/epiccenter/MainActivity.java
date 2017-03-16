c649dcf562c7a563006315e54045b27886f485b1
George Nguyen <g.nguyen92@yahoo.com>
2017-03-16 14:53:33 -0700

    Update MainActivity to create/inflate a clickable menu.

--- app/src/main/java/com/example/gnguy/epiccenter/MainActivity.java
+++ app/src/main/java/com/example/gnguy/epiccenter/MainActivity.java
@@ -12,6 +12,8 @@ import android.app.LoaderManager.LoaderCallbacks;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -126,6 +128,29 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
         }
 
     }
+
+
+    // Override method to inflate the FilterActivity (App bar menu)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    // Set up FilterActivity to respond to clicks on the main menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_filter){
+            Intent filterIntent = new Intent(this, FilterActivity.class);
+            startActivity(filterIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 
     /**
      *  Update the UI with the given earthquake list information
