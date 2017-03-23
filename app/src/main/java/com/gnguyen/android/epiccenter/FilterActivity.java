87c0f3dbd5447a74b08602a0dd169c59ee274418
George Nguyen <g.nguyen92@yahoo.com>
2017-03-23 15:59:28 -0700

    Add current minMag and maxMag summaries to filter menu.

--- app/src/main/java/com/gnguyen/android/epiccenter/FilterActivity.java
+++ app/src/main/java/com/gnguyen/android/epiccenter/FilterActivity.java
@@ -1,7 +1,10 @@
 package com.gnguyen.android.epiccenter;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 
@@ -10,7 +13,7 @@ import android.widget.EditText;
  */
 
 // Create a filter fragment that opens a new activity when clicked
-public class FilterActivity extends AppCompatActivity {
+public class FilterActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -18,12 +21,37 @@ public class FilterActivity extends AppCompatActivity {
         setContentView(R.layout.filter_activity);
     }
 
+
     // set filter fragment to open a menu of filters
-    public static class EarthquakePreferenceFragment extends PreferenceFragment {
+    public static class EarthquakePreferenceFragment extends PreferenceFragment
+                                                implements Preference.OnPreferenceChangeListener {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.filter_main);
+
+            Preference minMagnitude = findPreference(getString(R.string.filter_min_mag_key));
+            bindPreferenceSummaryToValue(minMagnitude);
+
+            Preference maxMagnitude = findPreference(getString(R.string.filter_max_mag_key));
+            bindPreferenceSummaryToValue(maxMagnitude);
+            
+        }
+
+        // Set up Preference to detect when a preference is changed and display the current
+        // preference setting as the summary
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference){
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
         }
     }
 }
